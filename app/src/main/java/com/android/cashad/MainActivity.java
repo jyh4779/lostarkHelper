673001f2daea75cd7fa94979503218eeccce66bd
JYH <jyh4779@gmail.com>
2023-02-01 17:05:20 +0900

    First Project Commit

---
+++ app/src/main/java/com/android/cashad/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.cashad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+    }
+}
---
+++ app/src/main/java/com/android/cashad/ScreenReceiver.java
@@ -0,0 +1,18 @@
+package com.android.cashad;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ScreenReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+
+    }
+}
+
